5f61b9a50cf425aa42fc17c850acc47f802db2f7
ayyubxon <ayyubxonxayotxonov77@gmail.com>
2025-08-11 18:38:01 +0500

    i

---
+++ app.js
@@ -0,0 +1,24 @@
+import {
+  showAge,
+  addNumbers,
+  greet,
+  fullName,
+  perimetr,
+  triangleSides,
+  squareNumber,
+  animalInfo,
+  sayHello,
+  printName,
+} from "./function.js";
+
+// Funksiyalarni ishlatish
+showAge(20);
+addNumbers(23, 7);
+greet("Ayyubxon");
+fullName("Ayyubxon", "Xayotxonov");
+perimetr(23, 7);
+triangleSides(12, 42, 23);
+squareNumber(23);
+animalInfo("Mushuk", 3);
+sayHello();
+printName("Ali", "Abduvali", "Ahmad", "Hoji", "Ayyubxon");
---
+++ function.js
@@ -0,0 +1,55 @@
+// 1-masala
+export function showAge(age) {
+  console.log(`Sizning yoshingiz ${age}`);
+}
+
+// 2-masala
+export function addNumbers(num1, num2) {
+  console.log(num1 + num2);
+}
+
+// 3-masala
+export function greet(ism) {
+  console.log(`Salom ${ism}`);
+}
+
+// 4-masala
+export function fullName(firstName, lastName) {
+  console.log(firstName, lastName);
+}
+
+// 5-masala
+export function perimetr(l, w) {
+  console.log(2 * (l + w));
+}
+
+// 6-masala
+export function triangleSides(a, b, c) {
+  console.log(`a: ${a}`);
+  console.log(`b: ${b}`);
+  console.log(`c: ${c}`);
+}
+
+// 7-masala
+export function squareNumber(number) {
+  console.log(Math.pow(number, 2));
+}
+
+// 8-masala
+export function animalInfo(name, age) {
+  console.log(`Bu ${name} ${age} yoshda`);
+}
+
+// 9-masala
+export function sayHello() {
+  console.log("Salom dunyo");
+}
+
+// 10-masala
+export function printName(name1, name2, name3, name4, name5) {
+  console.log(name1);
+  console.log(name2);
+  console.log(name3);
+  console.log(name4);
+  console.log(name5);
+}
